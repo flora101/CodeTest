14bd3f6df7e76f61fed8d64a7dc3291e9bae48e5
배근영 <94815293+flora101@users.noreply.github.com>
2023-08-07 13:12:53 +0900

    Create 1874.py

---
+++ 스택큐/1874.py
@@ -0,0 +1,29 @@
+count = 1
+temp = True
+stack = []
+op = []
+
+N = int(input())
+for i in range(N):
+    num = int(input())
+    # num이하 숫자까지 스택에 넣기
+    while count <= num:
+        stack.append(count)
+        op.append('+')
+        count += 1
+
+    # num이랑 스택 맨 위 숫자가 동일하다면 제거
+    if stack[-1] == num:
+        stack.pop()
+        op.append('-')
+    # 스택 수열을 만들 수 없으므로 NO
+    else:
+        temp = False
+        break
+
+if temp == False:
+    print("NO")
+else:
+    for i in op:
+        print(i)
+#
