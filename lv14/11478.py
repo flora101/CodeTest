21a1e8002902d0c3024cc29d639b0b1d1e83030f
배근영 <94815293+flora101@users.noreply.github.com>
2023-08-07 13:33:31 +0900

    Delete lv14 directory

--- lv14/11478.py
+++
@@ -1,7 +0,0 @@
-s=str(input())
-res=set()
-for i in range(len(s)):
-    for j in range(len(s)-i):
-        res.add(s[i:j+i+1])
-print(len(res))
-#
--- lv14/1269.py
+++
@@ -1,5 +0,0 @@
-a,b=map(int,input().split())
-arr1=set(map(int,input().split()))
-arr2=set(map(int,input().split()))
-print(len(arr1^arr2))
-#
